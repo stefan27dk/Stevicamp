37b6a501704c717998727a3a98034ea9b0a49122
Admin <Admin@BG-LA1-MSI>
2025-01-21 22:28:17 +0200

    database

---
+++ resources/db/database.js
@@ -0,0 +1,13 @@
+
+// The 1 file db with all the products as objects in the object.
+var db = {
+    appliances: [],
+    caravans: [{id:"1D2025-01-21T19-57-54.913Z!txj5btx8wa", brand:"Knaus", model:"Sunshine 500", year:"2002", length:"500", date:"2025-01-21 22:08 ч.", condition:"Използвано", toilet:"Химическа", heating:"Газово", ac:"Няма", sleepingPlaces:"6", fortelt:"Да", markise:"Не" ,description:"Good caravan - description", photos:["https://cdn.jsdelivr.net/gh/stefan27dk/Stevicamp@main/resources/img/Caravans/Caravan-Knaus-540D2025-01-21T17-59-45.662Z!iuarh18q78/1D2025-01-21T18-44-41.342Z!u77tt1ppa9.jpg","https://cdn.jsdelivr.net/gh/stefan27dk/Stevicamp@main/resources/img/Caravans/Caravan-Knaus-540D2025-01-21T17-59-45.662Z!iuarh18q78/2D2025-01-21T18-46-56.767Z!dlzte64rx6.jpg","https://cdn.jsdelivr.net/gh/stefan27dk/Stevicamp@main/resources/img/Caravans/Caravan-Knaus-540D2025-01-21T17-59-45.662Z!iuarh18q78/3D2025-01-21T18-47-15.110Z!33kdqox9e1.jpg","https://cdn.jsdelivr.net/gh/stefan27dk/Stevicamp@main/resources/img/Caravans/Caravan-Knaus-540D2025-01-21T17-59-45.662Z!iuarh18q78/4D2025-01-21T18-47-29.788Z!4jwyhyn2j3.jpg","https://cdn.jsdelivr.net/gh/stefan27dk/Stevicamp@main/resources/img/Caravans/Caravan-Knaus-540D2025-01-21T17-59-45.662Z!iuarh18q78/5D2025-01-21T18-47-43.020Z!n85kvz81on.jpg"]}],
+    cars: [],
+    equipment: [],
+    microbuses: [],
+    products: [],
+    scooters: [],
+    trailers: [],
+    wheels: [],
+  };
