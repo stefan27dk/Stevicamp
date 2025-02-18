9fc353b7e166e7c425f80360f889e8b70aaa5dbd
stefan27dk <stefan27dkm>
2025-02-18 21:00:05 +0200

    Added {cache: "reload"}

--- static/js/views/Common.js
+++ static/js/views/Common.js
@@ -13,7 +13,7 @@ var db = null;
 
 export async function getDb() {
     if (db == null) {
-        var jsDb = await fetch('https://cdn.jsdelivr.net/gh/stefan27dk/Stevicamp@latest/resources/db/database.json?1')
+        var jsDb = await fetch('https://cdn.jsdelivr.net/gh/stefan27dk/Stevicamp@latest/resources/db/database.json?1', {cache: "reload"})
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
