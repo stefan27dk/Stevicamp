60a751083ed02c8e13dcc303d740752e182f4fdb
stefan27dk <stefan27dkm>
2025-03-20 20:50:18 +0200

    ok

--- static/js/views/Caravans.js
+++ static/js/views/Caravans.js
@@ -36,7 +36,7 @@ export async function getHtmlAsync() {
 //     }
 //  // Да се направи общ код, този по-горе.
 //     return  ` <p class="subTitleView"> Каравани</p> ${items}`;
-    return  ` <p class="subTitleView"> Каравани</p> ${getItems("caravans")}`;
+    return  ` <p class="subTitleView"> Каравани</p> ${Common.getItems("caravans")}`;
                 
 
 }
