75a393bf706acd6db13ccfa26e68501fccbf72ea
stefan27dk <stefan27dkm>
2025-04-24 23:08:55 +0300

    Looks like the new algoritm for search is not nececery. Global search does not work.

--- static/js/resources/default_scripts.js
+++ static/js/resources/default_scripts.js
@@ -289,49 +289,51 @@ async function searchCurrentItems(e)
  
 // ####### Search Script ###########################################################################
 
-async function deepSearch(data, searchTxt) 
-{ 
-    let searchTxtFirstChar = searchTxt[0];
-    var searchResult = "";
+// It looks like this is not nececery because the .includes(match) does the search. obj[p].toLocaleLowerCase().includes(match).
+// async function deepSearch(data, searchTxt) 
+// { 
+//     let searchTxtFirstChar = searchTxt[0];
+//     var searchResult = "";
 
-    if (searchTxtFirstChar !== undefined) // If not empty string
-    {
-        for (let t = 0; t < data.length; t++) // Loop data to check each char and compare with the Search text first char
-        {
-            if (searchTxtFirstChar == data[t]) // Compare data with search txt first char. If there is match 
-            {
-                for (let y = 0; y < searchTxt.length; y++) // Try to match the search word with the next chars in the data
-                {
+//     if (searchTxtFirstChar !== undefined) // If not empty string
+//     {
+//         for (let t = 0; t < data.length; t++) // Loop data to check each char and compare with the Search text first char
+//         {
+//             if (searchTxtFirstChar == data[t]) // Compare data with search txt first char. If there is match 
+//             {
+//                 for (let y = 0; y < searchTxt.length; y++) // Try to match the search word with the next chars in the data
+//                 {
 
-                    if (searchTxt[y] == data[t + y]) // Try to match the search word from the data provided
-                    {
-                        searchResult += data[t + y]; // Add the matched char to searchResult string [n],[a],[m],[e]
-                    }
-                    else // if there was no word match // Reset the result to be ready for the next try // At this point here there have been no char match, no full match
-                    {
-                        searchResult = ""; // There is no full match, but partly only some chars, thats why reset the searchResult to continue to the new try, no need to check further there was no match, but partial match let say nam was matched but we need name to be matched as whole word 
-                        break;
-                    }
-                    if (searchResult.length == searchTxt.length) // Check if there is a whole match
-                    {
-                        return true; // Stop everything and return true - there was match
-                    }
-                }
-            }
+//                     if (searchTxt[y] == data[t + y]) // Try to match the search word from the data provided
+//                     {
+//                         searchResult += data[t + y]; // Add the matched char to searchResult string [n],[a],[m],[e]
+//                     }
+//                     else // if there was no word match // Reset the result to be ready for the next try // At this point here there have been no char match, no full match
+//                     {
+//                         searchResult = ""; // There is no full match, but partly only some chars, thats why reset the searchResult to continue to the new try, no need to check further there was no match, but partial match let say nam was matched but we need name to be matched as whole word 
+//                         break;
+//                     }
+//                     if (searchResult.length == searchTxt.length) // Check if there is a whole match
+//                     {
+//                         return true; // Stop everything and return true - there was match
+//                     }
+//                 }
+//             }
 
-        }
-        return false; // No match, At this point the whole data was looped and there is no match
-    }
-    else 
-    {
-        return false;
-    }
-}
+//         }
+//         return false; // No match, At this point the whole data was looped and there is no match
+//     }
+//     else 
+//     {
+//         return false;
+//     }
+// }
 
 
 // Search Object
 async function searchObject(obj, match) 
  {
+    match = match.toLocaleLowerCase();
     for (const p in obj) {
         let type = typeof obj[p];
 
@@ -341,11 +343,7 @@ async function searchObject(obj, match)
             if (obj[p].toLocaleLowerCase().includes(match) === true) 
             {
                 return obj;
-            }
-            else if(await deepSearch(obj[p].toLocaleLowerCase(), match)) // Match case
-            {
-               return obj;
-            }
+            } 
         }
         // Int, Float, Bool, BigInt
         else if (type === 'number' || type === 'boolean' || type === 'bigint') {
@@ -386,6 +384,7 @@ async function searchObject(obj, match)
 // Search Array
 async function searchArray(arr, match)
 {
+    match = match.toLocaleLowerCase();
     console.time();
     let resultArr = [];
     for (let b = arr.length; b--;) {
@@ -400,7 +399,7 @@ async function searchArray(arr, match)
         else if (type === 'string') {
             if (arr[b].toLocaleLowerCase().includes(match) === true) {
                 resultArr.push({ stringValue: arr[b] });
-            }
+            } 
         }
         // Int, Float, Bool, BigInt
         else if (type === 'number' || type === 'boolean' || type === 'bigint') {
