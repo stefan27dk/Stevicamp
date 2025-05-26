d95fc916f235f77b6e35fa178ec834708bf33381
stefan27dk <stefan27dkm>
2025-05-26 21:19:28 +0300

    Ok

--- static/js/views/TransportService.js
+++ static/js/views/TransportService.js
@@ -5,7 +5,7 @@ import * as Common from "./Common.js"
 
 // VIEW HTML ---------------------------------------------------
 export async function getHtmlAsync() {
-    return  `${await getItems('transportService')}`;
+    return  ``;
 }
 
 
