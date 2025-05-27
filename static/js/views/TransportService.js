a9dec57ee379b38df413f08c064d50c8c6eabe66
stefan27dk <stefan27dkm>
2025-05-27 09:42:04 +0300

    Transport services ok

--- static/js/views/TransportService.js
+++ static/js/views/TransportService.js
@@ -5,11 +5,19 @@ import * as Common from "./Common.js"
 
 // VIEW HTML ---------------------------------------------------
 export async function getHtmlAsync() {
-    return  ``;
+    let db =  await getDb();
+    return  `<p style="color: rgb(66, 215, 245);"><u>Транспорт и пътна помощ на автомобили:</u></p>
+
+    <img class="transport-service-img" src="static/img/recovery-truck-and-trailer.png" />
+    </br>
+    <p class="contact-information">Ако преглеждате сайта от телефон, натиснете върху дадения телефон за да звъннете, автоматично.</p>
+    </br>
+    <div class="about">${phoneViberNumberInfoHtml(db.phone, db.viberPhone)}</div>
+    <hr>
+    `;
 }
 
-
-
+ 
 
 // View Script -------------------------------------------------
 export async function executeViewScriptAsync() {
